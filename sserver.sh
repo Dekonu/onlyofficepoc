4e980b71198d793027e4ba9d696b17e9243e3b6a
Jigar Gor <jigar.gor@saama.com>
2024-03-04 10:41:31 -0800

    fixed typo in script to start server

--- sserver.sh
+++ sserver.sh
@@ -4,4 +4,4 @@ export DOCUMENT_SERVER_PRIVATE_URL='http://localhost:80'
 export DOCUMENT_SERVER_PUBLIC_URL='http://localhost:80'
 export EXAMPLE_URL='http://host.docker.internal:8000'
 
-python3  manage.py run-server
+python3  manage.py runserver

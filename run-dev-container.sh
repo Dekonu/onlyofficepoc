b81d8758508589bba3bef0d1dd9b6208d8ff7c0f
Jigar Gor <jigar.gor@saama.com>
2024-03-04 09:50:54 -0800

    removed gz files, added command to run dev docker container

---
+++ run-dev-container.sh
@@ -0,0 +1,6 @@
+docker run -i -t -d -p 80:80 --restart=always \
+-v $pwd/app/onlyoffice/documentserver/data:/var/www/onlyoffice/Data \
+-e ALLOW_META_IP_ADDRESS=true \
+-e ALLOW_PRIVATE_IP_ADDRERSS=true \
+-e WOPI_ENABLED=true \
+onlyoffice/documentserver-de

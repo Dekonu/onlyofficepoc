b62b9efa9f863b1aeb24c1da51adfecbee85c29a
Jigar Gor <jigar.gor@saama.com>
2024-03-04 11:19:27 -0800

    prepping to develop on the ec2 with docker

--- sserver.sh
+++ sserver.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-export DOCUMENT_SERVER_PRIVATE_URL='http://localhost:80'
-export DOCUMENT_SERVER_PUBLIC_URL='http://localhost:80'
-export EXAMPLE_URL='http://host.docker.internal:8000'
+export DOCUMENT_SERVER_PRIVATE_URL=http://localhost:8000
+export DOCUMENT_SERVER_PUBLIC_URL=http://10.4.8.109:80
+export EXAMPLE_URL=http://localhost:8000
+export JWT_SECRET="$(cat .secret)"
 
-python3  manage.py runserver
+
+python3 manage.py runserver
